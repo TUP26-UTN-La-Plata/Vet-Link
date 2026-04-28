1e968202ee82dfddc10635c5796e0689b998cafc
tincho1980 <miramallo@gmail.com>
2026-04-28 00:35:27 -0300

    base styles, folders, route and layout

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,26 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'login',
+    title: 'Vet-Link - Inicio de Sesión',
+    loadComponent: () => import('./features/login/login').then(m => m.Login)
+  },
+  {
+    path: '',
+    loadComponent: () => import('./shared/layout/layout').then(m => m.Layout),
+    children: [
+      {
+        path: 'patients',
+        title: 'Vet-Link - Gestión de Pacientes',
+        loadComponent: () => import('./features/patients/patients').then(m => m.Patients)
+      },
+      {
+        path: 'settings',
+        title: 'Vet-Link - Configuración',
+        loadComponent: () => import('./features/settings/settings').then(m => m.Settings)
+      },
+      { path: '', redirectTo: 'patients', pathMatch: 'full' }
+    ]
+  }
+];
