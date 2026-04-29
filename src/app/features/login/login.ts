0a142de318fb086d0bf68c2e33e7200cb6b0cdad
Kano55K <joaquinrafaelmiro@gmail.com>
2026-04-29 10:12:09 -0300

    logo-image added

--- src/app/features/login/login.ts
+++ src/app/features/login/login.ts
@@ -20,32 +20,19 @@ export class Login implements OnInit {
   ngOnInit(): void {
     this.checkExistingSession();
   }
-
-  /**
-   * Verifica si existe una sesión activa
-   * Si existe, redirige a la vista principal
-   */
   private checkExistingSession(): void {
     if (this.authService.isLoggedIn()) {
       this.router.navigate(['/patients']);
     }
   }
 
-  /**
-   * Maneja el click del botón de inicio de sesión
-   * Simula una verificación de 2 segundos y luego crea una sesión
-   */
   handleLogin(): void {
     if (this.isLoading()) return; // Evita múltiples clics
 
     this.isLoading.set(true);
 
-    // Simula verificación de usuario por 2 segundos
     setTimeout(() => {
-      // Crear sesión mediante el servicio
       this.authService.login();
-
-      // Redirigir a la vista principal
       this.isLoading.set(false);
       this.router.navigate(['/patients']);
     }, 2000);
