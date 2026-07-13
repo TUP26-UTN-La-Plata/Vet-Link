a8ef55302774e1d471f99a24e7a3a3e7798fbc74
Tati <santiagomatheu@gmail.com>
2026-07-13 09:11:11 -0300

    new configuration in lighthouserc.js to tell github actios not to search for static files

--- lighthouserc.js
+++ lighthouserc.js
@@ -2,6 +2,7 @@ module.exports = {
   ci: {
     collect: {
       numberOfRuns: 1,
+      startServerCommand: 'echo "Skipping server start"',
     },
     upload: {
       target: 'temporary-public-storage',
