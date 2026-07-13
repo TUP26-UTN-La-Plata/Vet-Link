30751cd896cc032cfcf749ae10ce5ee40e4284ed
Tati <santiagomatheu@gmail.com>
2026-07-13 09:17:59 -0300

    new configuration in lighthouserc.js

--- lighthouserc.js
+++ lighthouserc.js
@@ -2,7 +2,7 @@ module.exports = {
   ci: {
     collect: {
       numberOfRuns: 1,
-      startServerCommand: 'echo "Skipping server start"',
+      staticDistDir: './dist/Vet-Link',
     },
     upload: {
       target: 'temporary-public-storage',
